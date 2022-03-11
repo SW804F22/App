737e6dec3c19a69d3a6e1225ec503eddb60eb995
LJensen0403 <Lasseon.Jensen@gmail.com>
2022-03-11 09:16:41 +0100

    Environment Variables

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_login/app.dart';
 import 'package:user_repository/user_repository.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-Future main() async {
-  await dotenv.load(fileName: ".env");
+void main() async {
+  //Load env variables
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
 
   runApp(App(
     authenticationRepository: AuthenticationRepository(),
