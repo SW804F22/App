604241910a034e63d8b40fd5a7aee223447ef554
AdrianPlesner <adrian.plesner@gmail.com>
2022-03-15 11:46:20 +0100

    Ios api key fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,21 @@
 import UIKit
 import Flutter
 import flutter_config
+import google_maps_flutter
+
+
+
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  FlutterConfigPlugin.env(for: "MAPS_API_KEY");
-  NSString* mapsApiKey = [[NSProcessInfo processInfo] environment[@"MAPS_API_KEY"];
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(mapsApiKey)
+    var mapsApiKey = FlutterConfigPlugin .env(for: "MAPS_API_KEY");
+    GMSServices.provideAPIKey(mapsApiKey!)
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
