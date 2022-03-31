3c5baf41dff16f439fbf923b948ff83bfcbb48b0
Oliversrensen <ossa18@student.aau.dk>
2022-03-31 14:58:10 +0200

    Love this

--- lib/GlobalStyles.dart
+++ lib/GlobalStyles.dart
@@ -7,8 +7,8 @@ final Color secondary = Color(0xffBBBFCA);
 final Color tertiary = Color(0xffE8E8E8);
 final Color fourth = Color(0xffF4F4F2);
 
-final double fontBig = 18.0;
-final double fontMedium = 16.0;
-final double fontSmall = 12.0;
+const double fontBig = 18.0;
+const double fontMedium = 16.0;
+const double fontSmall = 12.0;
 
 
