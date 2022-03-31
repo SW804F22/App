700345e5b8a8bcd72ae7de0841fb8ef035978d6a
Oliversrensen <ossa18@student.aau.dk>
2022-03-31 14:12:22 +0200

    Love this

---
+++ lib/GlobalStyles.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class GlobalStyles{
+
+  final Color primary = Color(0xff495464);
+  final Color secondary = Color(0xffBBBFCA);
+  final Color tertiary = Color(0xffE8E8E8);
+  final Color fourth = Color(0xffF4F4F2);
+
+  final int fontBig = 18;
+  final int fontMedium = 16;
+  final int fontSmall = 12;
+
+}
